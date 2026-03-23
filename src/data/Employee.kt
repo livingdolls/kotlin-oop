d48040079af5ba4e6de02bce242270ff3ad3e0f7
livingdolls <nanangyorozuya@gmail.com>
2026-03-23 07:24:56 +0700

    overriding

--- src/data/Employee.kt
+++ src/data/Employee.kt
@@ -1,11 +1,26 @@
 package data
 
 open class Employee(val name: String) {
-    fun sayHello(name: String) {
+    open fun sayHello(name: String) {
         println("Hello $name!, my name is ${this.name}")
     }
 }
 
-class Manager(name: String) : Employee(name)
+open class Manager(name: String) : Employee(name) {
+    final override fun sayHello(name: String) {
+        println("Hello $name!, my name is Manager ${this.name}")
+    }
+}
 
-class VicePresident(name: String) : Employee(name)
+class SuperManager(name: String) : Manager(name) {
+//    Ini error karena fungsi override adalah final
+//    override fun sayHello(name: String) {
+//
+//    }
+}
+
+class VicePresident(name: String) : Employee(name) {
+    override fun sayHello(name: String) {
+        println("Hello $name!, my name is Vice President ${this.name}")
+    }
+}
