f6542ded177fc8e8b6554be99517e91e838ba608
livingdolls <nanangyorozuya@gmail.com>
2026-03-28 14:00:59 +0700

    observarble properties

--- src/data/Account.kt
+++ src/data/Account.kt
@@ -1,8 +1,15 @@
 package data
 
-class Account {
+import kotlin.properties.Delegates
+
+class Account(description: String = "") {
     val name: String by lazy {
         println("Hello, !")
         "Nanang"
+    }
+
+    var description: String by Delegates.observable(description) {
+        property, oldValue, newValue ->
+        println("${property.name} change from $oldValue to $newValue")
     }
 }
